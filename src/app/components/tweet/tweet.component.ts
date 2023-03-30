2d00df2c1a6e27e0304915fad87ee27be110eb06
Pedro <pedro.mello@virtus.ufcg.edu.br>
2023-03-30 19:21:48 -0300

    adding the tweet component layout. getting the tweets from local storage to populate the timeline

--- src/app/components/tweet/tweet.component.ts
+++ src/app/components/tweet/tweet.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {User} from "../../models/user";
+import {Tweet} from "../../models/tweet";
 
 @Component({
   selector: 'app-tweet',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./tweet.component.scss']
 })
 export class TweetComponent {
-
+  @Input() user!: User;
+  @Input() tweets: Tweet[] = [];
 }
